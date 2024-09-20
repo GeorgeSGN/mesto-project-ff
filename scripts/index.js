623c7220165100a3a5ca03a10f78f199dd2716f8
George Skornyakov <George.SGN@yandex.ru>
2024-09-20 22:56:13 +0300

    на проверку

--- scripts/index.js
+++ scripts/index.js
@@ -1,9 +1,42 @@
-// @todo: Темплейт карточки
+// Получаем ссылку на список карточек и шаблон.
+const cardList = document.querySelector('.places__list');
+const cardTemplate = document.querySelector('#card-template').content.querySelector('.card');
 
-// @todo: DOM узлы
+// Функция создания новой карточки.
+function createCard(cardData) {
+  // Клонируем шаблон карточки
+  const cardElement = cardTemplate.cloneNode(true);
 
-// @todo: Функция создания карточки
+  // Находим элементы карточки
+  const cardImage = cardElement.querySelector('.card__image');
+  const cardTitle = cardElement.querySelector('.card__title');
+  const deleteButton = cardElement.querySelector('.card__delete-button');
 
-// @todo: Функция удаления карточки
+  // Заполняем данными карточки
+  cardImage.src = cardData.link;
+  cardImage.alt = `Фото ${cardData.name}`;
+  cardTitle.textContent = cardData.name;
 
-// @todo: Вывести карточки на страницу
+  // Добавляем обработчик на кнопку удаления
+  deleteButton.addEventListener('click', () => {
+    deleteCard(cardElement);
+  });
+
+  // Возвращаем готовый элемент карточки
+  return cardElement;
+}
+
+// Функция удаления карточки 
+function deleteCard(cardElement) {
+  cardElement.remove();
+}
+
+// Функция добавления всех карточек на страницу
+function renderCards() {
+  initialCards.forEach(cardData => {
+    const cardElement = createCard(cardData);
+    cardList.append(cardElement);
+  })
+}
+
+renderCards();
